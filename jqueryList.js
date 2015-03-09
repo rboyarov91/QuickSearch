0e298df93c56aed3083af517faa2a75609313d4f
Roman Boyarov <rboyarov91@gmail.com>
2015-03-09 15:54:32 -0400

    popup updates
    updated scripts and stylesheets for autofill textfield

--- jqueryList.js
+++ jqueryList.js
@@ -24,16 +24,6 @@
       "Scala",
       "Scheme"
     ];
-    $( "#tags" ).keyup(function(event){
-    if(event.keyCode == 13){
-        //save to clipboard
-      $( "#tags" ).focus();
-
-      $( "#tags" ).execCommand('SelectAll');
-
-      $( "#tags" ).execCommand("Copy",false,null);
-    }
-});
     $( "#tags" ).autocomplete({
       source: availableTags
     });
