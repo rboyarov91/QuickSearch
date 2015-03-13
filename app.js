f26a38677fe49945baaeb6815db8b7ea05d33e9e
Roman Boyarov <rboyarov91@gmail.com>
2015-03-13 17:41:59 -0400

    added store() and restore() and modularity to options js
    Made the options js code more modular and added mostly empty functions
    for store and restore that still need to be populated and called.

--- app.js
+++ app.js
@@ -3,27 +3,7 @@ var main = function() {
 
     var comment = $('.comment').val(); //value in command box
     var command = $('.command').val(); //value in comment box
-    var listItem = $('<li>');
-    //Create table and add class
-    var table = $('<table></table>');
-    table.addClass('myTable');
-    //create first row and add to table
-    var firstRow = $('<tr></tr>')
-    firstRow.addClass('firstRow');
-    firstRow.append('<td><b>' + command + '</b></td>');
-    table.append(firstRow);
-    //create second row and add to table
-    var secondRow = $('<tr></tr>')
-    secondRow.addClass('secondRow');
-    secondRow.append('<td>' + comment + '</td>');
-    table.append(secondRow);
-    //add table to list item and then to list
-    listItem.append(table);
-    listItem.prependTo('.posts');
-
-
-
-
+    populateTable({command:command,comment:comment});
     $('.status-box').val('');
     $('.btn').addClass('disabled'); 
 
@@ -43,4 +23,38 @@ var main = function() {
   $('.btn').addClass('disabled');
 }
 
+
 $(document).ready(main);
+
+var restore  = function(array){
+  for(i = 0;i<array.length;i++){
+    populateTable(array[i]);
+  }
+}
+
+var save = function(item){
+  //add to the array of stored items in chrome
+}
+
+
+var populateTable = function(item){
+  var command = item.command;
+  var comment = item.comment;
+  var listItem = $('<li>');
+    //Create table and add class
+    var table = $('<table></table>');
+    table.addClass('myTable');
+    //create first row and add to table
+    var firstRow = $('<tr></tr>')
+    firstRow.addClass('firstRow');
+    firstRow.append('<td><b>' + command + '</b></td>');
+    table.append(firstRow);
+    //create second row and add to table
+    var secondRow = $('<tr></tr>')
+    secondRow.addClass('secondRow');
+    secondRow.append('<td>' + comment + '</td>');
+    table.append(secondRow);
+    //add table to list item and then to list
+    listItem.append(table);
+    listItem.prependTo('.posts');
+}
